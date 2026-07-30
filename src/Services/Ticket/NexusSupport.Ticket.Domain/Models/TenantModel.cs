fe8a03491fb514e4f3025e1e9c27fc8442e17c3d
Luis Alberto Sandoval Monestel <79773016+LuisiitoDev@users.noreply.github.com>
2026-07-29 20:00:00 -0600

    Remove Tenant-related classes and interfaces to streamline the application structure

--- src/Services/Ticket/NexusSupport.Ticket.Domain/Models/TenantModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NexusSupport.Ticket.Domain.Models;
-
-public class TenantModel
-{
-    public Guid Id { get; set; }
-    public required string Name { get; set; }
-    public bool IsActive { get; set; }
-    public DateTime CreateAt { get; set; }
-}
