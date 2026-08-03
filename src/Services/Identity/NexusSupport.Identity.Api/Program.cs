54e07bea72a70934e7a734ef205133769fbc97c2
LuisiitoDev <luisasandovalcoder@gmail.com>
2026-08-02 19:37:41 -0600

    feat: Add Scalar.AspNetCore package and configure Scalar API reference

--- src/Services/Identity/NexusSupport.Identity.Api/Program.cs
+++ src/Services/Identity/NexusSupport.Identity.Api/Program.cs
@@ -1,6 +1,6 @@
 using NexusSupport.Identity.Application.Extensions;
 using NexusSupport.Identity.Infrastructure.Extensions;
-
+using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
@@ -12,6 +12,14 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.MapScalarApiReference("/docs", options =>
+    {
+        options
+            .WithTitle("Nexus Identity")
+            .WithTheme(ScalarTheme.Mars)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    });
 }
 
 app.UseHttpsRedirection();
